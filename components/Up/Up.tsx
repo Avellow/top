217538659e72498e63f95d4827ed5645f399913e
Avellow <avellow@yandex.ru>
2023-01-25 12:39:20 +0300

    feature создать компонент ButtonIcon и рефакторинг комп. up

--- components/Up/Up.tsx
+++ components/Up/Up.tsx
@@ -3,6 +3,7 @@ import UpIcon from './up.svg';
 import {useScrollY} from "../../hooks/useScrollY";
 import {motion, useAnimation} from "framer-motion";
 import {useEffect} from "react";
+import {ButtonIcon} from "../ButtonIcon/ButtonIcon";
 
 export const Up = (): JSX.Element => {
     const controls = useAnimation();
@@ -20,13 +21,12 @@ export const Up = (): JSX.Element => {
     };
 
     return (
-        <motion.button
+        <motion.div
             className={styles.up}
-            onClick={scrollToTop}
             animate={controls}
             initial={{ opacity: 0 }}
         >
-            <UpIcon />
-        </motion.button>
+            <ButtonIcon appearance='primary' icon='up' onClick={scrollToTop} />
+        </motion.div>
     );
 };
