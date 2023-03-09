4b727db19c044fa4979fb3ef16e01d9de9261945
Avellow <avellow@yandex.ru>
2023-03-09 16:29:36 +0300

    сборка docker

--- components/Up/Up.tsx
+++ components/Up/Up.tsx
@@ -1,32 +1,32 @@
 import styles from './Up.module.css';
 import UpIcon from './up.svg';
-import {useScrollY} from "../../hooks/useScrollY";
-import {motion, useAnimation} from "framer-motion";
-import {useEffect} from "react";
-import {ButtonIcon} from "../ButtonIcon/ButtonIcon";
+import { useScrollY } from "../../hooks/useScrollY";
+import { motion, useAnimation } from "framer-motion";
+import { useEffect } from "react";
+import { ButtonIcon } from "../ButtonIcon/ButtonIcon";
 
 export const Up = (): JSX.Element => {
-    const controls = useAnimation();
-    const y = useScrollY();
+  const controls = useAnimation();
+  const y = useScrollY();
 
-    useEffect(() => {
-        controls.start({ opacity: y / document.body.scrollHeight });
-    }, [y, controls]);
+  useEffect(() => {
+    controls.start({ opacity: y / document.body.scrollHeight });
+  }, [y, controls]);
 
-    const scrollToTop = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
-    };
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
 
-    return (
-        <motion.div
-            className={styles.up}
-            animate={controls}
-            initial={{ opacity: 0 }}
-        >
-            <ButtonIcon appearance='primary' icon='up' onClick={scrollToTop} />
-        </motion.div>
-    );
+  return (
+    <motion.div
+      className={styles.up}
+      animate={controls}
+      initial={{ opacity: 0 }}
+    >
+      <ButtonIcon appearance='primary' icon='up' aria-label='Наверх' onClick={scrollToTop} />
+    </motion.div>
+  );
 };
